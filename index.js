6d7ffb8ec20ef61bdcbe6693efe9545d903a2f0e
tengyuan <2010402073@qq.com>
2025-02-18 15:39:27 +0800

    3-16test

--- index.js
+++ index.js
@@ -1,58 +1,164 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const app = express();
-const Person = require('./models/person')
+// require('dotenv').config();
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const app = express();
+// const Person = require('./models/person')
 
-// 检查命令行参数
-if (process.argv.length < 3) {
-    console.log('give password as argument');
-    process.exit(1);
-}
 
-const password = process.argv[2];
-const name = process.argv[3];
-const number = process.argv[4];
-
-// const url = process.env.MONGODB_URI;
-
-// mongoose.set('strictQuery', false);
-// mongoose.connect(url);
-
-// // 定义数据模型
-// const personSchema = new mongoose.Schema({
-//     name: String,
-//     number: String,
+// app.get('/api/persons', (request, response) => {
+//     Person.find({})
+//         .then(persons => {
+//             response.json(persons);
+//         })
+//         .catch(error => {
+//             console.error('Error fetching persons:', error);
+//             response.status(500).send('Internal Server Error');
+//         });
 // });
+// //创建新联系人
+// app.post('/api/persons', (request, response) => {
+//     const body = request.body
 
-// const Person = mongoose.model('Person', personSchema);
-
-// //格式化返回对象
-// personSchema.set('toJSON', {
-//     transform: (document, returnedObject) => {
-
-//         delete returnedObject._id
-//         delete returnedObject.__v
+//     if (body.content === undefined) {
+//         return response.status(400).json({ error: 'content missing' })
 //     }
+
+//     const person = new Person({
+//         content: body.content,
+//         important: body.important || false,
+//     })
+
+//     person.save().then(savedPerson => {
+//         response.json(savedPerson)
+//     })
 // })
 
+// const PORT = process.env.PORT;
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+// });
 
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
+require('dotenv').config();
+const Person = require('./models/person');
 
-// 查询所有
+app.use(express.static('build'));
+app.use(express.json());
+app.use(cors());
+app.use(morgan('tiny'));
+
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method);
+    console.log('Path:  ', request.path);
+    console.log('Body:  ', request.body);
+    console.log('---');
+    next();
+};
+
+app.use(requestLogger);
+
+// 获取所有联系人
 app.get('/api/persons', (request, response) => {
-    Person.find({})
-        .then(persons => {
-            response.json(persons);
-        })
-        .catch(error => {
-            console.error('Error fetching persons:', error);
-            response.status(500).send('Internal Server Error');
-        });
+    Person.find({}).then(persons => {
+        response.json(persons);
+    });
 });
+
+// 获取单个联系人
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person);
+            } else {
+                response.status(404).end();
+            }
+        })
+        .catch(error => next(error));
+});
+
+// 删除单个联系人
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(() => {
+            response.status(204).end();
+        })
+        .catch(error => next(error));
+});
+
+// 创建新联系人
+app.post('/api/persons', (request, response, next) => {
+    const body = request.body;
+
+    if (body.name === undefined || body.number === undefined) {
+        return response.status(400).json({ error: 'name or number missing' });
+    }
+
+    const person = new Person({
+        name: body.name,
+        number: body.number,
+    });
+
+    person.save()
+        .then(savedPerson => {
+            response.json(savedPerson);
+        })
+        .catch(error => next(error));
+});
+
+// 更新联系人
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body;
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    };
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson);
+        })
+        .catch(error => next(error));
+});
+
+// 未知端点处理
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' });
+};
+
+app.use(unknownEndpoint);
+
+// 错误处理中间件
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' });
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message });
+    }
+
+    next(error);
+};
+
+app.use(errorHandler);
+
+// 连接数据库
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log('connected to MongoDB');
+    })
+    .catch((error) => {
+        console.log('error connecting to MongoDB:', error.message);
+    });
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
-
